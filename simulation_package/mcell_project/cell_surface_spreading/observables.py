fe0e511e60f27124d180aba49127dfb117ada2a4
jchaiken12 <jonahc12@yahoo.com>
2025-08-05 14:25:37 +0300

    Add files via upload

---
+++ simulation_package/mcell_project/cell_surface_spreading/observables.py
@@ -0,0 +1,21 @@
+import mcell as m
+
+from parameters import *
+from subsystem import *
+from geometry import *
+
+# ---- observables ----
+
+viz_output = m.VizOutput(
+    mode = m.VizMode.ASCII,
+    output_files_prefix = './viz_data/seed_' + str(get_seed()).zfill(5) + '/Scene',
+    every_n_timesteps = 10
+)
+
+# declaration of rxn rules defined in BNGL and used in counts
+
+observables = m.Observables()
+observables.add_viz_output(viz_output)
+
+# load observables information from bngl file
+observables.load_bngl_observables('model.bngl', './react_data/seed_' + str(get_seed()).zfill(5) + '/')
